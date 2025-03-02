bcb09c98f7e50d55edcd03163b39e59ba0c27f79
rihan <rihanalkamim@gmail.com>
2025-03-02 04:44:47 -0300

    Dependencies and YAML

--- Vaultwarden-Installation.sh
+++ Vaultwarden-Installation.sh
@@ -1,2 +1,33 @@
 #!/bin/bash
 
+#Ubuntu Version
+
+#Installation of dependencies
+apt update -y && apt upgrade -y
+	#Install Docker
+	apt install -y docker docker-compose
+	#Install OpenSSL and Nginx (Reverse Proxy)
+	apt install -y openssl nginx
+
+#Inputs
+echo -n "Your domain (Ex: intern.domain): "
+read -r domain
+
+#Enviroments
+
+#Install Vaultwarden
+mkdir -p /Vaultwarden && cd /Vaultwarden
+
+printf \
+"services:\t
+   vaultwarden:\t
+     image: vaultwarden/server:latest\t
+     container_name: vaultwarden\t
+     restart: unless-stopped\t
+     environment:\t
+       DOMAIN: $domain\t
+     volumes:\t
+       - ./vw-data/:/data/\t
+     ports:\t
+       - 80:80" \
+> compose.yaml
